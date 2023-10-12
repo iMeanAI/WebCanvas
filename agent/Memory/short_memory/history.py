ba774c9882fe4f58589124c25b47796f80e904bb
zsd <sida@stellarrover.com>
2023-10-12 06:48:00 +0000

    build up the history memory

--- agent/Memory/short_memory/history.py
+++ agent/Memory/short_memory/history.py
@@ -1,15 +1,19 @@
 import json5
+from Prompt import *
 
 class HistoryMemory:
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self,previous_trace) -> None:
+        """
+        previous_trace : list of each previous action and thought info before current environment state
+        """
+        self.previous_trace = previous_trace
         
-    def stringfy_thought_and_action(self,input_list):
+    def stringfy_thought_and_action(self):
         """
-        input_list : thought and actiton for each step
-        return : output after stringfy
+        return : output after stringfy previous trace
         """
-        input_list = json5.loads(input_list, encoding="utf-8")
+        input_list = json5.loads(self.previous_trace, encoding="utf-8")
         str_output = "["
         for idx, i in enumerate(input_list):
             str_output += f'Step{idx+1}:\"Thought: {i["thought"]}, Action: {i["action"]}\";\n'
@@ -19,3 +23,11 @@ class HistoryMemory:
         return str_output
 
     
+    def get_previous_trace_prompt(self):
+        # return prompt_user += eval(previous_trace_prompt)
+        previous_trace_prompt = f"The previous thoughts and actions are: {self.stringfy_thought_and_action()}.\n\nYou have done the things above.\n\n"
+        return previous_trace_prompt
+
+
+    def get_cache_trace(self):
+        pass
