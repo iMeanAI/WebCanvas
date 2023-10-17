4952dc564b24eb68b73caa571d6c9edb0bb0018a
PYC <506149387@qq.com>
2023-10-17 16:04:48 +0800

    add comments

--- agent/Plan/planning.py
+++ agent/Plan/planning.py
@@ -8,14 +8,17 @@ class Planning:
 
     @staticmethod
     async def plan(uuid, user_request, tab_name_list, current_tab_name, current_time, previous_trace, dom):
+        # 创建GPT查询类
         GPT35 = GPTGenerator35()
         GPT4 = GPTGenerator4()
 
+        # 构建planning prompt及查询
         planning_request = PlanningPromptConstructor().construct(
             user_request, previous_trace, dom, tab_name_list, current_tab_name)
 
         response = GPT35.request(planning_request)
 
+        # 构建reward prompt及查询
         stringfy_thought_and_action_output = PlanningPromptConstructor().stringfy_thought_and_action(previous_trace)
 
         reward_request = RewardPromptConstructor().construct(user_request, stringfy_thought_and_action_output) 
--- agent/Prompt/prompt_constructor.py
+++ agent/Prompt/prompt_constructor.py
@@ -9,7 +9,7 @@ class BasePromptConstructor:
         pass
 
 
-class PlanningPromptConstructor(BasePromptConstructor):
+class PlanningPromptConstructor(BasePromptConstructor): # 类：构建planning的prompt
     def __init__(self):
         self.prompt_system = BasePrompts.planning_prompt_system
         self.prompt_user = BasePrompts.planning_prompt_user
@@ -31,13 +31,14 @@ class PlanningPromptConstructor(BasePromptConstructor):
                 f"link element: {str(dom_denoised.link_element)}\n\n"\
                 f"input elements(like input and textarea): {str(dom_denoised.input_element)}"
             if len(dom_denoised.unknown_element) > 0:
-                prompt_user += f"\n\nother elements with tagname: {str(dom_denoised.unknown_element)}"
+                self.prompt_user += f"\n\nother elements with tagname: {str(dom_denoised.unknown_element)}"
 
         messages = [{"role": "system", "content": self.prompt_system}, {
             "role": "user", "content": self.prompt_user}]
         return messages
 
-    def stringfy_thought_and_action(input_list):
+    # 将previous thought和action转化成格式化字符串
+    def stringfy_thought_and_action(input_list: list) -> str:
         input_list = json5.loads(input_list, encoding="utf-8")
         str_output = "["
         for idx, i in enumerate(input_list):
@@ -48,11 +49,12 @@ class PlanningPromptConstructor(BasePromptConstructor):
         return str_output
 
 
-class RewardPromptConstructor(BasePromptConstructor):
+class RewardPromptConstructor(BasePromptConstructor): # 类：构建reward的prompt
     def __init__(self):
         self.prompt_system = BasePrompts.reward_prompt_system
         self.prompt_user = BasePrompts.reward_prompt_user
 
+    # 构建reward的prompt，输出openai可解析的格式
     def construct(self, user_request: str, stringfy_thought_and_action_output: str) -> list:
         self.prompt_user = Template(self.prompt_user).render(
             user_request=user_request, stringfy_thought_and_action_output=stringfy_thought_and_action_output)
@@ -61,13 +63,14 @@ class RewardPromptConstructor(BasePromptConstructor):
         return messages
 
 
-class JudgeSearchbarPromptConstructor(BasePromptConstructor):
+class JudgeSearchbarPromptConstructor(BasePromptConstructor): # 类：构建判断该元素是否是搜索框的prompt（如果是，则前端需要额外加上回车操作）
     def __init__(self):
         self.prompt_system = BasePrompts.judge_searchbar_prompt_system
         self.prompt_user = BasePrompts.judge_searchbar_prompt_user
 
+    # 构建判断是否是搜索框的prompt，输出openai可解析的格式
     # TODO 改掉decoded_result
-    def constructor(self, user_request: str, input_element, decoded_result) -> list:
+    def constructor(self, input_element, decoded_result) -> list:
         self.prompt_user = Template(self.prompt_user).render(input_element=str(
             input_element), element_id=decoded_result['element_id'], action_input=decoded_result['action_input'])
         messages = [{"role": "system", "content": self.prompt_system}, {
