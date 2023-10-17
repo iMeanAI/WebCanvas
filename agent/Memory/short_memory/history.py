88294637960b4c94095bee80590032b0e04bcc54
zsd <sida@stellarrover.com>
2023-10-17 13:06:30 +0000

    update history.py

--- agent/Memory/short_memory/history.py
+++ agent/Memory/short_memory/history.py
@@ -1,16 +1,15 @@
 import json5
-# from Prompt import *
+
+
 class HistoryMemory:
 
     def __init__(self,previous_trace: list) -> None:
-
         """
-        previous_trace : list of each previous action and thought info before current environment state
+        previous_trace: list of each previous action and thought info before current environment state
         """
         self.previous_trace = previous_trace
         
     def stringfy_thought_and_action(self)->str:
-
         """
         output after stringfy previous trace
         """
@@ -19,22 +18,17 @@ class HistoryMemory:
         for idx, i in enumerate(input_list):
             str_output += f'Step{idx+1}:\"Thought: {i["thought"]}, Action: {i["action"]}\";\n'
         str_output += "]"
-        # logger.info(f"\033[32m\nstr_output\n{str_output}\033[0m")#绿色
-        # logger.info(f"str_output\n{str_output}")
         return str_output
-
     
     def ConstructPreviousTracePrompt(self)->str:
         """
         """
-        # return prompt_user += eval(previous_trace_prompt)
-
-        previous_trace_prompt = f"The previous thoughts and actions are: {self.stringfy_thought_and_action()}.\n\nYou have done the things above.\n\n"
+        stringfy_thought_and_action_output = self.stringfy_thought_and_action()
+        previous_trace_prompt = f"The previous thoughts and actions are: \
+            {stringfy_thought_and_action_output}.\n\nYou have done the things above.\n\n"
         return previous_trace_prompt
 
-
     def ConstructCacheTrace(self):
-
         """
         """
         pass
