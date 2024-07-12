e3668168619c55487578ce72c0cc5836a9cefd40
CarlC <icuicheng@foxmail.com>
2024-07-12 22:29:26 +0800

    add gemini.py

---
+++ agent/LLM/gemini.py
@@ -0,0 +1,43 @@
+import os
+import sys
+import asyncio
+from functools import partial
+import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+from sanic.log import logger
+import google.generativeai as genai
+
+gemini_api_key = os.getenv("GOOGLE_API_KEY")
+if not gemini_api_key:
+    logger.error("Gemini API key is missing. Please set the GOOGLE_API_KEY environment variable.")
+    sys.exit(1)
+
+genai.configure(api_key=gemini_api_key)
+
+
+class GeminiGenerator:
+    def __init__(self, model=None):
+        self.model = model
+        self.pool = ThreadPoolExecutor(max_workers=os.cpu_count() * 2)
+
+    async def request(self, messages: list = None, max_tokens: int = 500, temperature: float = 0.7) -> (str, str):
+        loop = asyncio.get_event_loop()
+        try:
+            response = await loop.run_in_executor(self.pool, partial(self.chat, messages, max_tokens, temperature))
+            return response, ""
+        except Exception as e:
+            logger.error(f"Error in GeminiGenerator.request: {e}")
+            return "", str(e)
+
+    def chat(self, messages, max_tokens=500, temperature=0.7):
+        chat_history = []
+        for message in messages:
+            chat_history.append({"role": "user", "parts": [{"text": message.get("content")}]})
+            # chat_history.append({"role": "model", "parts": [{"text": message.get("content")}]})
+        running_model = genai.GenerativeModel(self.model)
+        chat = running_model.start_chat(history=chat_history)
+        latest_user_message = messages[-1].get("content")
+        response = chat.send_message(latest_user_message, generation_config=genai.types.GenerationConfig(
+            max_output_tokens=max_tokens,
+            temperature=temperature))
+        return response.text
