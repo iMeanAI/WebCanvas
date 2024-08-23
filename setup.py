1eb3ee7478d2829b116d7c60329ced7e49238308
CarlC <icuicheng@foxmail.com>
2024-08-23 17:27:30 +0800

    update setup.py for this agent

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+
+# dynamically load dependencies in requirements.txt
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file if line and not line.startswith("#")]
+
+
+setup(
+    name="WebCanvas",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    author="Cheng Cui, Sida Zhou, Dehan Kong, Yichen Pan",
+    author_email="dehan@imean.ai",
+    description="A universal agent framework with four key modules: Planning, Observation, Memory, and Reward, designed to perform complex tasks within real-world online web environments effectively.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/iMeanAI/WebCanvas",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
